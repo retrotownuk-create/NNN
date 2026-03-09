8dff6fcd703933700be65ddbf3541995e5e8ef29
abdul bari noori <abdulbarinoori@MacBook-Pro.local>
2026-03-09 21:04:41 +0000

    feat(sku168): recreate sturdy H-frame 3-flange foot base from sku126, incorporating 5-Way Hubs, half-height Unions, and a bottom crossbar to perfectly match cutlist constraints

--- src/App.tsx
+++ src/App.tsx
@@ -5086,65 +5086,94 @@ const Rack = ({ length, height, wallDistance, explode, hasShelves = true, isFree
   }
 
   if (skuType === 'sku168') {
+    // Modular freestanding rack with 5-Way Hub bases.
     const e = explode * 1.5;
-    const baseArmHeight = 4.4; // Flange(1.2) + HexNipple(1.0 + 2.2 collar spacing)
+    const baseArmHeight = 5.75;
+    const spreadArm = 23; // wallDistance / 2 ... wait, sku126 uses 23. sku168 cutlist uses spreadArm = wallDistance / 2.
+    // Let's use wallDistance / 2
     const unionHeight = (height + baseArmHeight) / 2;
-    const spreadArm = wallDistance / 2;
 
-    const buildLeg = (x: number, isLeft: boolean) => {
-      const xExp = isLeft ? -e : e;
-      // Inward direction for the 5-way side branch
-      const innerRot: [number, number, number] = isLeft ? [0, 0, 0] : [0, Math.PI, 0];
+    const buildLeg = (x, type) => {
+      const isLeft = type === 'left';
+      const sideX = isLeft ? -e : e;
+      const innerDir = isLeft ? 1 : -1;
+      const outerDir = isLeft ? -1 : 1;
+
+      // 5-Way Hub rotation: the "side" branch (+X locally) rotated by 90deg Z becomes UP (+Y)
+      // Local +Y becomes -X (left). Local -Y becomes +X (right).
+      const hubRot = [0, 0, Math.PI / 2];
 
       return (
-        <group key={'leg' + x} position={[xExp, 0, 0]}>
-          {/* Top Half of Leg */}
-          <group position={[0, e, 0]}>
-            <Elbow position={[x, height, 0]} rotation={[0, 0, isLeft ? -Math.PI / 2 : Math.PI / 2]} showLabel={showLabel} colorOption={colorOption} />
+        <group key={type} position={[sideX, 0, 0]}>
+          {/* Top Corner Elbow */}
+          <group position={[0, e * 3, 0]}>
+            <Elbow position={[x, height, 0]} rotation={[0, isLeft ? Math.PI / 2 : -Math.PI / 2, 0]} showLabel={showLabel} colorOption={colorOption} />
+          </group>
+
+          {/* Vertical Pole - Top Half */}
+          <group position={[0, e * 2, 0]}>
             <Pipe start={[x, height - 1.2, 0]} end={[x, unionHeight + 1.2, 0]} showLabel={showLabel} colorOption={colorOption} />
-            <Coupling position={[x, unionHeight, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
           </group>
 
-          {/* Bottom Half of Leg */}
+          {/* Union */}
+          <group position={[0, e * 1.5, 0]}>
+            <Union position={[x, unionHeight, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+          </group>
+
+          {/* Vertical Pole - Bottom Half */}
+          <group position={[0, e, 0]}>
+            <Pipe start={[x, unionHeight - 1.2, 0]} end={[x, baseArmHeight + 1.2, 0]} showLabel={showLabel} colorOption={colorOption} />
+          </group>
+
+          {/* 5-Way Junction Base */}
           <group position={[0, 0, 0]}>
-            <Pipe start={[x, unionHeight - 1.2, 0]} end={[x, baseArmHeight + 2.2, 0]} showLabel={showLabel} colorOption={colorOption} />
+            <FiveWayFitting position={[x, baseArmHeight, 0]} rotation={hubRot} showLabel={showLabel} colorOption={colorOption} />
           </group>
 
-          {/* 5-Way Central Hub Base */}
-          <group position={[0, -e, 0]}>
-            <FiveWayFitting position={[x, baseArmHeight, 0]} rotation={innerRot} showLabel={showLabel} colorOption={colorOption} />
-
-            {/* Center Foot */}
-            <group position={[0, -e * 0.5, 0]}>
-              <HexNipple position={[x, baseArmHeight - 2.3, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
-            </group>
-            <group position={[0, -e * 1.0, 0]}>
-              <Flange position={[x, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
-            </group>
-
-            {/* Front Foot */}
-            <group position={[0, 0, e]}>
-              <Pipe start={[x, baseArmHeight, 2.2]} end={[x, baseArmHeight, spreadArm - 1.2]} showLabel={showLabel} colorOption={colorOption} />
-              <Elbow position={[x, baseArmHeight, spreadArm]} rotation={[0, Math.PI, 0]} showLabel={showLabel} colorOption={colorOption} />
-
-              <group position={[0, -e * 0.5, 0]}>
-                <HexNipple position={[x, baseArmHeight - 2.3, spreadArm]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
-              </group>
-              <group position={[0, -e * 1.0, 0]}>
-                <Flange position={[x, 0, spreadArm]} rotation={[-Math.PI / 2, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+          {/* Outward Stabilizer Arm & Foot (Hex Nipple horizontally + Drop) */}
+          <group position={[outerDir * e * 0.5, 0, 0]}>
+            <HexNipple position={[x + outerDir * 2.875, baseArmHeight, 0]} rotation={[0, 0, Math.PI / 2]} showLabel={showLabel} colorOption={colorOption} />
+            <group position={[x + outerDir * 5.75, baseArmHeight, 0]}>
+              <group position={[outerDir * e * 0.5, 0, 0]}>
+                <Elbow position={[0, 0, 0]} rotation={[0, isLeft ? -Math.PI / 2 : Math.PI / 2, 0]} showLabel={showLabel} colorOption={colorOption} />
+                <group position={[0, -e * 0.5, 0]}>
+                  <HexNipple position={[0, -2.875, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+                  <group position={[0, -e * 0.5, 0]}>
+                    <Flange position={[0, -5.75, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+                  </group>
+                </group>
               </group>
             </group>
+          </group>
 
-            {/* Back Foot */}
-            <group position={[0, 0, -e]}>
-              <Pipe start={[x, baseArmHeight, -2.2]} end={[x, baseArmHeight, -spreadArm + 1.2]} showLabel={showLabel} colorOption={colorOption} />
-              <Elbow position={[x, baseArmHeight, -spreadArm]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
-
-              <group position={[0, -e * 0.5, 0]}>
-                <HexNipple position={[x, baseArmHeight - 2.3, -spreadArm]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+          {/* Forward Stabilizer Arm & Foot */}
+          <group position={[0, 0, e * 0.5]}>
+            <Pipe start={[x, baseArmHeight, 2.0]} end={[x, baseArmHeight, spreadArm - 2.0]} showLabel={showLabel} colorOption={colorOption} />
+            <group position={[x, baseArmHeight, spreadArm]}>
+              <group position={[0, 0, e * 0.5]}>
+                <Elbow position={[0, 0, 0]} rotation={[0, Math.PI, 0]} showLabel={showLabel} colorOption={colorOption} />
+                <group position={[0, -e * 0.5, 0]}>
+                  <HexNipple position={[0, -2.875, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+                  <group position={[0, -e * 0.5, 0]}>
+                    <Flange position={[0, -5.75, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+                  </group>
+                </group>
               </group>
-              <group position={[0, -e * 1.0, 0]}>
-                <Flange position={[x, 0, -spreadArm]} rotation={[-Math.PI / 2, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+            </group>
+          </group>
+
+          {/* Backward Stabilizer Arm & Foot */}
+          <group position={[0, 0, -e * 0.5]}>
+            <Pipe start={[x, baseArmHeight, -2.0]} end={[x, baseArmHeight, -spreadArm + 2.0]} showLabel={showLabel} colorOption={colorOption} />
+            <group position={[x, baseArmHeight, -spreadArm]}>
+              <group position={[0, 0, -e * 0.5]}>
+                <Elbow position={[0, 0, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+                <group position={[0, -e * 0.5, 0]}>
+                  <HexNipple position={[0, -2.875, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+                  <group position={[0, -e * 0.5, 0]}>
+                    <Flange position={[0, -5.75, 0]} rotation={[0, 0, 0]} showLabel={showLabel} colorOption={colorOption} />
+                  </group>
+                </group>
               </group>
             </group>
           </group>
@@ -5152,27 +5181,25 @@ const Rack = ({ length, height, wallDistance, explode, hasShelves = true, isFree
       );
     }
 
-    const hPipeLength = length - 3.0; // matches cutlist and collar spacing
-
     return (
       <group position={[0, -height / 2, 0]}>
-        {buildLeg(-(length / 2 - 1.5), true)}
-        {buildLeg((length / 2 - 1.5), false)}
+        {buildLeg(-(length / 2 - 1.5), 'left')}
+        {buildLeg((length / 2 - 1.5), 'right')}
 
         {/* Top Rail */}
-        <group position={[0, e, 0]}>
+        <group position={[0, e * 3, 0]}>
           <Pipe start={[-(length / 2 - 3.0), height, 0]} end={[(length / 2 - 3.0), height, 0]} showLabel={showLabel} colorOption={colorOption} />
         </group>
 
         {/* Bottom Rail connecting 5-Way Hubs */}
-        <group position={[0, -e, 0]}>
+        <group position={[0, 0, 0]}>
           <Pipe start={[-(length / 2 - 3.0), baseArmHeight, 0]} end={[(length / 2 - 3.0), baseArmHeight, 0]} showLabel={showLabel} colorOption={colorOption} />
         </group>
       </group>
     );
   }
 
-  if (skuType === 'sku173') {
+  if (skuType === 'sku173')  {
     const e = explode * 2.0;
     const bracketZ = -wallDistance;
     const lHalf = length / 2;
